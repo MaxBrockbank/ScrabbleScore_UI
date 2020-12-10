3ee897ffe231ed8493b1aea38225dad9630027c1
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-10 14:02:24 -0800

    implement CLI logic
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

--- ScrabbleScore/Program.cs
+++ ScrabbleScore/Program.cs
@@ -1,3 +1,19 @@
 using System;
 using System.Collections.Generic;
 
+
+namespace ScrabbleScore.Models
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("Scrabble Score Calculator");
+            Console.WriteLine("Please enter a word");
+            string userInput = Console.ReadLine();
+            Game newGame = new Game(userInput);
+            Console.WriteLine(newGame.PlayerScore());
+
+        }
+    }
+}
