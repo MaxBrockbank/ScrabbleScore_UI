6b0f922813704e85d522ceda36c4f4ede3f6fbee
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-10 10:13:48 -0800

    setup project file structure
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

---
+++ ScrabbleScore.Tests/ModelTests/GameTests.cs
@@ -0,0 +1,17 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using ScrabbleScore.Models;
+
+namespace ScrabbleScore.Tests
+{
+  [TestClass]
+  public class GameTests : IDisposeable
+  {
+    public void Dispose()
+    {
+
+    }
+    
+  }
+}
