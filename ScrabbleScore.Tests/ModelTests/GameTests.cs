0684d32c85837788817dc77ad43f105eb766d20d
Max Brockbank <maxbrockbank1999@gmail.com>
2020-12-10 11:53:05 -0800

    write and good fail PlayerScore test #1
    Co-authored-by: Jeff Chiu <jeffchiudev@gmail.com>

--- ScrabbleScore.Tests/ModelTests/GameTests.cs
+++ ScrabbleScore.Tests/ModelTests/GameTests.cs
@@ -6,12 +6,35 @@ using ScrabbleScore.Models;
 namespace ScrabbleScore.Tests
 {
   [TestClass]
-  public class GameTests : IDisposeable
+  public class GameTests : IDisposable
   {
     public void Dispose()
     {
 
     }
-    
+
+    [TestMethod]
+    public void GameConstructor_NewGameObject_Game()
+    {
+        string userInput = "word";
+        Game newWord = new Game(userInput);
+        Assert.AreEqual(typeof(Game), newWord.GetType());
+    }
+
+    [TestMethod]
+    public void GameConstructor_CreatesCharacterArrayFromInput_Array()
+    {
+        string userInput = "word";
+        Game newWord = new Game(userInput);
+        Assert.AreEqual(typeof(char[]), newWord.ArrayForWord.GetType());
+    }
+
+    [TestMethod]
+    public void PlayerScore_CalculateScore_Int()
+    {
+        string userInput = "word";
+        Game newWord = new Game(userInput);
+        Assert.AreEqual(2, newWord.PlayerScore());
+    }
   }
 }
--- ScrabbleScore/Models/Game.cs
+++ ScrabbleScore/Models/Game.cs
@@ -1,10 +1,26 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace ScrabbleScore.Models
 {
   public class Game
   {
-    
+    public char[] ArrayForWord {get; set;}
+    public string UserWord {get; set;}
+    private static string onePointPattern = @"aeioulnrst";
+    public Regex onePoint = new Regex(onePointPattern, RegexOptions.IgnoreCase);
+
+    public Game (string word)
+    {
+      UserWord = word;
+      ArrayForWord = word.ToCharArray();
+    }
+
+    public int PlayerScore()
+    {
+      return 0;
+    }
+
   }
 }
